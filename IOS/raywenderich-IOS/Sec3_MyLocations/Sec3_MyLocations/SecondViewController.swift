7be9394cbd1fa056668cf3dd3f8e20ff0f823f68
onemoon <onemoonstudio@gmail.com>
2018-12-23 21:51:40 +0900

    Feat : Ch 23 raywendrich

---
+++ IOS/raywenderich-IOS/Sec3_MyLocations/Sec3_MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Sec3_MyLocations
+//
+//  Created by Hyeontae on 22/12/2018.
+//  Copyright © 2018 onemoon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IOS/raywenderich-IOS/section3_myLocations/README.md
@@ -0,0 +1,11 @@
+
+# CH 21 : swift review
+
+- variables , constants , and types
+- methods and functions 
+- making decisions
+- loops
+- objects
+- protocols 
+
+
