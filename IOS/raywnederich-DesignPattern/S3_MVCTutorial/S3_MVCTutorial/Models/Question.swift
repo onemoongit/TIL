7845094d0944ea9ffd16eb050f441a12a86455f6
onemoon <onemoonstudio@gmail.com>
2019-01-18 14:09:13 +0900

    Feat : Raywendrich DesignPattern
    Feat
    Sections 3 MVC Pattern Tutorial

---
+++ IOS/raywnederich-DesignPattern/S3_MVCTutorial/S3_MVCTutorial/Models/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  S3_MVCTutorial
+//
+//  Created by Hyeontae on 18/01/2019.
+//  Copyright © 2019 onemoon. All rights reserved.
+//
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
